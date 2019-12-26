43902950e007211aca76c9592fb3e8acf4f9029e
miku86 <miku86coding@gmail.com>
2019-12-26 15:52:04 +0100

    Reorder and declutter

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "typeface-roboto";
-import App from "./App";
+import App from "./components/App";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -12,8 +11,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
