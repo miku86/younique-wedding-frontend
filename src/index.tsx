6e8a6dff4395fd354efb2275b5a90760a9651ffe
miku86 <miku86coding@gmail.com>
2019-12-27 18:08:01 +0100

    Add amplify to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,30 @@
+import Amplify from "aws-amplify";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "typeface-roboto";
 import App from "./components/App";
+import config from "./config";
 import "./index.css";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "younique-wedding",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      }
+    ]
+  }
+});
 
 ReactDOM.render(
   <BrowserRouter>
