220a02adfb5225222769d17795296f9f2ce32d43
miku86 <miku86coding@gmail.com>
2019-12-28 09:55:14 +0100

    Add login

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
 
 interface Props {}
@@ -9,6 +10,7 @@ const Routes: React.FC<Props> = () => {
   return (
     <Switch>
       <Route path="/" exact={true} component={Home} />
+      <Route path="/login" exact={true} component={Login} />
       <Route component={NotFound} />
     </Switch>
   );
