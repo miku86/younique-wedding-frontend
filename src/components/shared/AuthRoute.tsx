8e539a949037f8d040de60118ce05a29a06a87ff
miku86 <miku86coding@gmail.com>
2020-01-04 12:19:01 +0100

    Create route to check if user is authed

---
+++ src/components/shared/AuthRoute.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { IappProps } from "../../utils/customTypes";
+
+interface Props {
+  path: string;
+  exact: boolean;
+  component: any;
+  appProps: IappProps;
+}
+
+const AuthRoute: React.FC<Props> = ({ component: C, appProps, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        appProps.isAuthenticated ? (
+          <C {...props} {...appProps} />
+        ) : (
+          <Redirect
+            to={`/login?redirect=${props.location.pathname}${props.location.search}`}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default AuthRoute;
