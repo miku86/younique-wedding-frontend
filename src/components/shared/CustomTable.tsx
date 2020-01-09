00a3d49477f317452ae114ea4daea38c6a8ce263
miku86 <miku86coding@gmail.com>
2020-01-09 17:50:31 +0100

    Add styling and column

--- src/components/shared/CustomTable.tsx
+++ src/components/shared/CustomTable.tsx
@@ -1,5 +1,15 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
+import { makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Theme } from "@material-ui/core";
 import React from "react";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  head: {
+    backgroundColor: "#3f51b5;",
+    
+    "& th": {
+      color: "white"
+    }
+  }
+}));
 
 interface Todo {
   [key: string]: any;
@@ -12,24 +22,28 @@ interface Props {
 type Item = Todo;
 
 const CustomTable: React.FC<Props> = ({ data }) => {
+  const classes = useStyles();
+
   return (
     <TableContainer component={Paper}>
       <Table>
-        <TableHead>
+        <TableHead className={classes.head}>
           <TableRow>
             <TableCell>Title</TableCell>
-            <TableCell align="right">Created At</TableCell>
+            <TableCell align="right">Deadline</TableCell>
+            <TableCell align="right">Responsible</TableCell>
+            <TableCell align="right">Comment</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((item: Item) => (
-            <TableRow key={item.todoId}>
+            <TableRow key={item.SK}>
               <TableCell component="th" scope="item">
-                {item.content}
+                {item.title}
               </TableCell>
-              <TableCell align="right">
-                {new Date(item.createdAt).toLocaleString()}
-              </TableCell>
+              <TableCell align="right">{item.comment}</TableCell>
+              <TableCell align="right">{item.comment}</TableCell>
+              <TableCell align="right">{item.comment}</TableCell>
             </TableRow>
           ))}
         </TableBody>
