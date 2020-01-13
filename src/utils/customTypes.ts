1b4067e55f207af3d82fff76dae7046ac6276bcf
miku86 <miku86coding@gmail.com>
2020-01-13 18:18:41 +0100

    Add type

--- src/utils/customTypes.ts
+++ src/utils/customTypes.ts
@@ -7,3 +7,15 @@ export interface IappProps {
   isAuthenticated: TisAuthenticated;
   setIsAuthenticated: TsetIsAuthenticated;
 }
+
+export interface Todo {
+  PK: string;
+  SK: string;
+  todoId: string;
+  userId: string;
+  title: string;
+  comment: string;
+  deadline: string;
+  responsible: string;
+  timestamp: number;
+}
