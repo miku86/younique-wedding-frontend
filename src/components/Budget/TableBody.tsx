b9ae86fed545dd59af0f8f77df25e40a495d0c19
miku86 <miku86coding@gmail.com>
2020-01-22 12:22:33 +0100

    Refactor sort into own file

--- src/components/Budget/TableBody.tsx
+++ src/components/Budget/TableBody.tsx
@@ -2,43 +2,15 @@ import { makeStyles, TableBody, TableCell, TableRow, Theme } from "@material-ui/
 import { Delete } from "@material-ui/icons";
 import React from "react";
 import { BudgetItem } from "../../utils/customTypes";
+import { getSorting, stableSort } from "../../utils/helpers";
 import CheckingIcon from "../shared/CheckingIcon";
-import { Order } from "./Table";
+import { Order } from "../shared/TableHead";
 
 const useStyles = makeStyles((theme: Theme) => ({
   deleteButton: {
     cursor: "pointer"
   }
 }));
-
-function stableSort<T>(array: T[], cmp: (a: T, b: T) => number) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = cmp(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map(el => el[0]);
-}
-
-function desc<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getSorting<K extends keyof any>(
-  order: Order,
-  orderBy: K
-): (a: { [key in K]: any }, b: { [key in K]: any }) => number {
-  return order === "desc"
-    ? (a, b) => desc(a, b, orderBy)
-    : (a, b) => -desc(a, b, orderBy);
-}
 
 interface Props {
   data: BudgetItem[];
--- src/components/Guests/TableBody.tsx
+++ src/components/Guests/TableBody.tsx
@@ -2,6 +2,7 @@ import { makeStyles, TableBody, TableCell, TableRow, Theme } from "@material-ui/
 import { Delete } from "@material-ui/icons";
 import React from "react";
 import { Guest } from "../../utils/customTypes";
+import { getSorting, stableSort } from "../../utils/helpers";
 import CheckingIcon from "../shared/CheckingIcon";
 import { Order } from "../shared/TableHead";
 
@@ -10,35 +11,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     cursor: "pointer"
   }
 }));
-
-function stableSort<T>(array: T[], cmp: (a: T, b: T) => number) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = cmp(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map(el => el[0]);
-}
-
-function desc<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getSorting<K extends keyof any>(
-  order: Order,
-  orderBy: K
-): (a: { [key in K]: any }, b: { [key in K]: any }) => number {
-  return order === "desc"
-    ? (a, b) => desc(a, b, orderBy)
-    : (a, b) => -desc(a, b, orderBy);
-}
 
 interface Props {
   data: Guest[];
