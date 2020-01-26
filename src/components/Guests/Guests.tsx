c7648c0e2d62f9738ed50fdd10d2aed5addca18e
miku86 <miku86coding@gmail.com>
2020-01-26 15:09:18 +0100

    Add update functionality

--- src/components/Guests/Guests.tsx
+++ src/components/Guests/Guests.tsx
@@ -1,11 +1,11 @@
 import { Box, Fab, Link, makeStyles, Theme } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
 import { API } from "aws-amplify";
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link as RouterLink } from "react-router-dom";
 import { config } from "../../config";
-import { TisAuthenticated } from "../../utils/customTypes";
+import { GuestInputs, TisAuthenticated } from "../../utils/customTypes";
 import Landing from "../shared/Landing";
 import LoadingSpinner from "../shared/LoadingSpinner";
 import Summary from "./Summary";
@@ -30,13 +30,12 @@ const Guests: React.FC<Props> = ({ isAuthenticated }) => {
   const classes = useStyles();
   const [isLoading, setIsLoading] = useState(false);
   const [guests, setGuests] = useState([]);
-  const {t}= useTranslation()
+  const { t } = useTranslation();
 
   const fetchGuests = async () => {
     setIsLoading(true);
     const guests = await API.get(config.API.NAME, "/guests", {});
     setGuests(guests);
-    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -50,17 +49,17 @@ const Guests: React.FC<Props> = ({ isAuthenticated }) => {
       } catch (error) {
         alert(error);
       }
+      setIsLoading(false);
     })();
   }, [isAuthenticated]);
 
   const deleteGuest = async (guestId: string) => {
-    const body = {
-      guestId
-    };
-
     setIsLoading(true);
-    await API.del(config.API.NAME, "/guests", { body });
-    setIsLoading(false);
+    await API.del(config.API.NAME, "/guests", {
+      body: {
+        guestId
+      }
+    });
   };
 
   const handleDelete = async (guestId: string) => {
@@ -76,38 +75,46 @@ const Guests: React.FC<Props> = ({ isAuthenticated }) => {
     } catch (error) {
       alert(error.message);
     }
+    setIsLoading(false);
   };
 
-  const updateGuest = (
-    guestId: string,
-    fieldKey: string,
-    newFieldValue: boolean
-  ) => {
-    const body = {
-      guestId,
-      fieldKey,
-      newFieldValue
-    };
-
-    return API.put(config.API.NAME, "/guests", { body });
+  const updateGuest = (guestId: string, data: any) => {
+    setIsLoading(true);
+    API.put(config.API.NAME, "/guests", { body: { guestId, data } });
   };
 
-  const handleUpdate = async (
+  const handleUpdateBools = async (
     guestId: string,
     fieldKey: string,
     fieldValue: boolean
   ) => {
-    const newFieldValue = !fieldValue;
-
-    setIsLoading(true);
+    const data = {
+      [fieldKey]: !fieldValue
+    };
 
     try {
-      await updateGuest(guestId, fieldKey, newFieldValue);
+      await updateGuest(guestId, data);
       fetchGuests();
     } catch (error) {
       alert(error.message);
-      setIsLoading(false);
     }
+    setIsLoading(false);
+  };
+
+  const handleUpdateTexts = async (
+    event: FormEvent<HTMLFormElement>,
+    guestId: string,
+    fields: GuestInputs
+  ) => {
+    event.preventDefault();
+
+    try {
+      await updateGuest(guestId, fields);
+      fetchGuests();
+    } catch (error) {
+      alert(error.message);
+    }
+    setIsLoading(false);
   };
 
   const renderGuests = () => {
@@ -122,7 +129,8 @@ const Guests: React.FC<Props> = ({ isAuthenticated }) => {
             <CustomTable
               data={guests}
               handleDelete={handleDelete}
-              handleUpdate={handleUpdate}
+              handleUpdateBools={handleUpdateBools}
+              handleUpdateTexts={handleUpdateTexts}
             />
 
             <Box display="flex" my={2}>
--- src/components/Guests/Table.tsx
+++ src/components/Guests/Table.tsx
@@ -1,18 +1,8 @@
 import { Paper, Table, TableContainer } from "@material-ui/core";
-import React from "react";
-import { Guest } from "../../utils/customTypes";
+import React, { FormEvent, useState } from "react";
+import { Guest, GuestInputs } from "../../utils/customTypes";
 import ExtendedTableHead, { HeadCell, Order } from "../shared/TableHead";
 import ExtendedTableBody from "./TableBody";
-
-interface Data {
-  name: string;
-  sentSaveTheDate: boolean;
-  sentInvite: boolean;
-  receivedResponse: boolean;
-  coming: boolean;
-  comment: string;
-  options?: string;
-}
 
 const headCells: HeadCell[] = [
   { id: "name", sorting: true },
@@ -27,16 +17,26 @@ const headCells: HeadCell[] = [
 interface Props {
   data: Guest[];
   handleDelete: (guestId: string) => void;
-  handleUpdate: (
+  handleUpdateBools: (
     guestId: string,
     fieldKey: string,
     fieldValue: boolean
   ) => void;
+  handleUpdateTexts: (
+    event: FormEvent<HTMLFormElement>,
+    guestId: string,
+    fields: GuestInputs
+  ) => void;
 }
 
-const CustomTable: React.FC<Props> = ({ data, handleDelete, handleUpdate }) => {
-  const [order, setOrder] = React.useState<Order>("asc");
-  const [orderBy, setOrderBy] = React.useState<keyof Data>("name");
+const CustomTable: React.FC<Props> = ({
+  data,
+  handleDelete,
+  handleUpdateBools,
+  handleUpdateTexts
+}) => {
+  const [order, setOrder] = useState<Order>("asc");
+  const [orderBy, setOrderBy] = useState<keyof GuestInputs>("name");
 
   const handleRequestSort = (
     event: React.MouseEvent<unknown>,
@@ -46,6 +46,7 @@ const CustomTable: React.FC<Props> = ({ data, handleDelete, handleUpdate }) => {
     setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
+
   return (
     <TableContainer component={Paper}>
       <Table size="medium">
@@ -59,9 +60,9 @@ const CustomTable: React.FC<Props> = ({ data, handleDelete, handleUpdate }) => {
           data={data}
           order={order}
           orderBy={orderBy}
-          showDeleteButton={true}
           handleDelete={handleDelete}
-          handleUpdate={handleUpdate}
+          handleUpdateBools={handleUpdateBools}
+          handleUpdateTexts={handleUpdateTexts}
         />
       </Table>
     </TableContainer>
