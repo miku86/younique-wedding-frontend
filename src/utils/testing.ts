45935f63cfd50d6e4e9547aebbcf64fee6a31d9a
miku86 <miku86coding@gmail.com>
2020-04-24 10:51:50 +0200

    Make parentElement optional

--- src/utils/testing.ts
+++ src/utils/testing.ts
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
-const renderWithRouter = (ui: any, parentElement: string) => {
+const renderWithRouter = (ui: any, parentElement?: string) => {
   return {
     ...render(
       ui,
