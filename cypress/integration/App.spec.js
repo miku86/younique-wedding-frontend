2d4249ea6d01bb14ea43b677e2c01c882b61cc97
miku86 <miku86coding@gmail.com>
2020-04-28 15:46:09 +0200

    Lint

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,9 +1,9 @@
-describe('App', () => {
-  describe('state: logged out', () => {
-    it('should show the landing page and its fields', () => {
-      cy.visit('/');
-      cy.get('[data-testid="landing-title"]');
-      cy.get('[data-testid="landing-pitch"]');
+describe("App", () => {
+  describe("state: logged out", () => {
+    it("should show the landing page and its fields", () => {
+      cy.visit("/");
+      cy.get("[data-testid='landing-title']");
+      cy.get("[data-testid='landing-pitch']");
     });
   });
-})
+});
