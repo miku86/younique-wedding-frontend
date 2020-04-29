5b8b3d1287ce17d498af9b319dc377295d022ef4
miku86 <miku86coding@gmail.com>
2020-04-29 20:27:32 +0200

    Add E2E test for adding a todo

---
+++ cypress/integration/todos-user-can-add-todo.spec.js
@@ -0,0 +1,32 @@
+import { API, ROUTES } from "../../src/config";
+import { getRandomDate, getRandomNumberBetween } from "../../src/utils/helpers";
+
+describe("Todos: User can add a Todo", () => {
+  it("should add a todo", () => {
+    cy.server();
+
+    cy.route({
+      method: "POST",
+      url: `*${API.TODOS}`,
+    }).as("addTodo");
+
+    cy.visit(ROUTES.TODOSNEW);
+    cy.get("[data-testid='demo-account']").click();
+
+    const title = String(getRandomNumberBetween(10, 1000));
+    cy.get("[data-testid='todo-new-title']").type(title);
+    cy.get("[data-testid='todo-new-deadline']").type(getRandomDate());
+    cy.get("[data-testid='todo-new-responsible']").type(String(getRandomNumberBetween(10, 1000)));
+    cy.get("[data-testid='todo-new-comment']").type(String(getRandomNumberBetween(10, 1000)));
+    cy.get("[data-testid='todo-new-add']").click();
+
+    cy.wait("@addTodo")
+      .should((xhr) => {
+        expect(xhr.method).to.equal("POST");
+        expect(xhr.url).to.match(/\/todos$/);
+        expect(xhr.status).to.equal(200);
+      });
+
+    cy.get("[data-testid='page-todos']").contains(title);
+  });
+});
