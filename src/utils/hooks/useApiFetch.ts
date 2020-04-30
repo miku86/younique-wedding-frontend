99d3d0fd2acbfa180547ff079e11a14cdb57939d
miku86 <miku86coding@gmail.com>
2020-04-30 09:06:21 +0200

    Move actions into constants

--- src/utils/hooks/useApiFetch.ts
+++ src/utils/hooks/useApiFetch.ts
@@ -2,22 +2,26 @@ import { API as AMPLIFY } from "aws-amplify";
 import { useEffect, useReducer, useState } from "react";
 import { config } from "../../config";
 
+const FETCH_INIT = "FETCH_INIT";
+const FETCH_SUCCESS = "FETCH_SUCCESS";
+const FETCH_FAILURE = "FETCH_FAILURE";
+
 const fetchReducer = (state: any, action: any) => {
   switch (action.type) {
-    case "FETCH_INIT":
+    case FETCH_INIT:
       return {
         ...state,
         isLoading: true,
         isError: false,
       };
-    case "FETCH_SUCCESS":
+    case FETCH_SUCCESS:
       return {
         ...state,
         isLoading: false,
         isError: false,
         data: action.payload,
       };
-    case "FETCH_FAILURE":
+    case FETCH_FAILURE:
       return {
         ...state,
         isLoading: false,
@@ -38,12 +42,12 @@ export const useApiFetch = (initialUrl: string, initialData: any) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      dispatch({ type: "FETCH_INIT" });
+      dispatch({ type: FETCH_INIT });
       try {
         const result = await AMPLIFY.get(config.API.NAME, url, {});
-        dispatch({ type: "FETCH_SUCCESS", payload: result });
+        dispatch({ type: FETCH_SUCCESS, payload: result });
       } catch (error) {
-        dispatch({ type: "FETCH_FAILURE" });
+        dispatch({ type: FETCH_FAILURE });
       }
     };
     fetchData();
