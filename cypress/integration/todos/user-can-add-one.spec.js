369162415aeb1baa848f602a2c33f7c96b235ad9
miku86 <miku86coding@gmail.com>
2020-05-05 09:18:33 +0200

    Add e2e test

--- cypress/integration/todos/user-can-add-one.spec.js
+++ cypress/integration/todos/user-can-add-one.spec.js
@@ -1,25 +1,35 @@
 import { API, ROUTES } from "../../../src/config";
-import { getRandomDate, getRandomNumberBetween } from "../../../src/utils/helpers";
+import { createMockTodoNew } from "../../../src/utils/fixtures";
 
 describe("Todos: User can add a Todo", () => {
   it("should add a todo", () => {
-    cy.server();
+    const todo = createMockTodoNew();
+    const { title, deadline, responsible, comment } = todo;
+
+    cy.server({ force404: true });
+
+    cy.route({
+      method: "GET",
+      url: `*${API.TODOS}`,
+      response: []
+    });
 
     cy.route({
       method: "POST",
       url: `*${API.TODOS}`,
+      response: todo,
     }).as("addTodo");
 
     cy.visit(ROUTES.TODOSNEW);
     cy.get("[data-testid='demo-account']").click();
 
-    const title = String(getRandomNumberBetween(10, 1000));
+
     cy.get("[data-testid='todo-new-title']").type(title);
-    cy.get("[data-testid='todo-new-deadline']").type(getRandomDate());
-    cy.get("[data-testid='todo-new-responsible']").type(String(getRandomNumberBetween(10, 1000)));
-    cy.get("[data-testid='todo-new-comment']").type(String(getRandomNumberBetween(10, 1000)));
+    cy.get("[data-testid='todo-new-deadline']").type(deadline);
+    cy.get("[data-testid='todo-new-responsible']").type(responsible);
+    cy.get("[data-testid='todo-new-comment']").type(comment);
     cy.get("[data-testid='todo-new-add']").click();
 
-    cy.get("[data-testid='page-todos']").should("contain", title);
+    cy.wait("@addTodo").its("requestBody").should("deep.equal", todo);
   });
 });
